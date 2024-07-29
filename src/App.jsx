459fbbeacdd07a4068f312b31d6f1204f208c8e3
Ongamele <92509099+ongamele@users.noreply.github.com>
2024-07-29 23:30:15 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Dashboard, Auth } from "@/layouts";
+import {YeboPay } from "./pages/dashboard/yeboPay";
 
 function App() {
   return (
     <Routes>
+      <Route path="/yebo-pay/:accountNumber" element={<YeboPay/>} />
       <Route path="/dashboard/*" element={<Dashboard />} />
       <Route path="/account/*" element={<Dashboard />} />
       <Route path="/auth/*" element={<Auth />} />
